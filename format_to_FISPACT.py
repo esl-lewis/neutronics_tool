34bb632c7073e7e476279b094377f6010f57b422
py1sl <steven.lilley@stfc.ac.uk>
2019-02-15 14:33:16 +0000

    PEP8 improvements in output formaters

--- format_to_FISPACT.py
+++ format_to_FISPACT.py
@@ -3,18 +3,19 @@
 Formats the data from dataframe into FISPACT input file
 
 """
-import pandas as pd 
+import pandas as pd
 import numpy as np
 
 from format_from_EXCEL import formatExcel
 
 df = formatExcel('cyclemainoperationalparameters.xlsx')
 
+
 def currentTOflux(I):
     """ Converts beam current (µA) into flux
     """
-    I = I/1e6 #conversion from microamps to amps
-    qp = 1.6e-19 # charge of proton in Coulombs  
+    I = I/1e6  # conversion from microamps to amps
+    qp = 1.6e-19  # charge of proton in Coulombs
     flux = I / (qp)
     return flux
 
@@ -24,7 +25,7 @@ df = df.apply(lambda x: currentTOflux(x['Average µA']), axis=1)
 maxlen = len(df.index)-1
 
 df = df.values
-# Converts to numpy friendly values 
+# Converts to numpy friendly values
 
 countdays = []
 countx = 0
@@ -32,11 +33,12 @@ count0 = 0
 
 flux = []
 
-""" FISPACT input takes integer values (here in days) for when beam was on/off and 
-the beam flux for that set of dates. This loop extracts that information from the
-dataframe and appends it to the empty sets 'flux' and 'countdays'
+""" FISPACT input takes integer values (here in days) for when beam was on/off
+    and the beam flux for that set of dates. This loop extracts that
+    information from the dataframe and appends it to the empty sets 'flux'
+    and 'countdays'
 """
-for i in range(0,maxlen):
+for i in range(0, maxlen):
     if df[i] > 0 and (df[i]) == (df[i+1]):
         countx += 1
     elif df[i] > 0 and (df[i]) != (df[i+1]):
@@ -53,7 +55,7 @@ for i in range(0,maxlen):
         print('There is an error here!')
 
 startbeamON = 0
-#Checks to see if first day the beam was on or off
+# Checks to see if first day the beam was on or off
 if df[0] > 0:
     startbeamON = True
 elif df[0] == 0:
@@ -63,29 +65,27 @@ numRuns = len(countdays)
 
 
 # write to FISPACT input format
-# nuclear data and intital conditions are left to user 
-file = open("fispact_test.i","w")
+# nuclear data and intital conditions are left to user
+file = open("fispact_test.i", "w")
 
 file.write("<< -----get nuclear data----- >>")
-file.write("\n<< -----set initial conditions----- >>") 
+file.write("\n<< -----set initial conditions----- >>")
 file.write("\nFLUX " + str(df[0]))
 file.write("\nATOMS \nATOMS DOSE 1")
 
 file.write("\n<< -----irradiation phase----- >>")
 file.write("\nTIME " + str(countdays[0]))
-file.write("\nDAYS \nTAB1 41 \nATOMS" )
+file.write("\nDAYS \nTAB1 41 \nATOMS")
 
-for j in range(1,numRuns-2):
-    file.write("\nFLUX "+ str(flux[j]) ) 
+for j in range(1, numRuns-2):
+    file.write("\nFLUX " + str(flux[j]))
     file.write("\nTIME " + str(countdays[j]) + " DAYS")
-    file.write("\nATOMS")       
+    file.write("\nATOMS")
 
 file.write("\n<< -----cooling phase----- >>")
-file.write("\nFLUX "+ str(flux[numRuns-1]) )
-file.write("\nZERO \nNOSTABLE") 
+file.write("\nFLUX " + str(flux[numRuns-1]))
+file.write("\nZERO \nNOSTABLE")
 file.write("\nTIME " + str(countdays[numRuns-1]) + " DAYS")
-file.write("\nATOMS \nEND \n* END")    
+file.write("\nATOMS \nEND \n* END")
 
 file.close()
-
-
--- format_to_FLUKA.py
+++ format_to_FLUKA.py
@@ -1,6 +1,6 @@
-#FORMAT TO FLUKA
+# FORMAT TO FLUKA
 import re
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import datetime
 import matplotlib.pyplot as plt
@@ -8,17 +8,18 @@ import matplotlib.pyplot as plt
 from pandas import ExcelWriter
 from pandas import ExcelFile
 from math import log10, floor
- 
+
 
 from format_from_EXCEL import formatExcel
 
 df = formatExcel('cyclemainoperationalparameters.xlsx')
 
+
 def currentTOflux(I):
     """ Converts beam current (µA) into flux
     """
-    I = I/1e6 #conversion from microamps to amps
-    qp = 1.6e-19 # charge of proton in Coulombs  
+    I = I/1e6  # conversion from microamps to amps
+    qp = 1.6e-19  # charge of proton in Coulombs
     flux = I / (qp)
     return flux
 
@@ -29,7 +30,7 @@ df = df.apply(lambda x: currentTOflux(x['Average µA']), axis=1)
 maxlen = len(df.index)-1
 
 df = df.values
-# Converts to numpy friendly values 
+# Converts to numpy friendly values
 
 countdays = []
 countx = 0
@@ -40,7 +41,7 @@ flux = []
 """ This loop extracts no. of days beam was on/off and flux amplitude from
 days it was on and appends it to the empty sets 'flux' and 'countdays'
 """
-for i in range(0,maxlen):
+for i in range(0, maxlen):
     if df[i] > 0 and (df[i]) == (df[i+1]):
         countx += 1
     elif df[i] > 0 and (df[i]) != (df[i+1]):
@@ -59,6 +60,7 @@ for i in range(0,maxlen):
 # convert days into seconds
 countdays = [x * 24 * 60 * 60 for x in countdays]
 
+
 # round down both flux and second values to 3 sf
 def round_to_4sf(x):
     """ note: change value in front of int to change no. of sfs we round to
@@ -71,16 +73,15 @@ def round_to_4sf(x):
 countdays = list(map(round_to_4sf, countdays))
 flux = list(map(round_to_4sf, flux))
 
-# convert flux into scientific notation 
+
+# convert flux into scientific notation
 def format_E(x):
     if x == 0:
         return '0.0'
     else:
-        return'{:.2e}'.format(x)    
+        return'{:.2e}'.format(x)
 
 flux = list(map(format_E, flux))
-
-        
 
 print(countdays)
 print(flux)
@@ -88,72 +89,69 @@ print(flux)
 print(type(flux[0]))
 print(type(flux[1]))
 
-tot = len(countdays) 
+tot = len(countdays)
 
-#write to FLUKA file
+# write to FLUKA file
 
-file = open("fluka_test.i","r+")
-# **** put something here to overwrite previous file ****  
-#The conditionals are to satisfy FLUKAS input requirements.
-#These ensure beam flickers ON then OFF for corresponding values of flux
+file = open("fluka_test.i", "r+")
+# **** put something here to overwrite previous file ****
+# The conditionals are to satisfy FLUKAS input requirements.
+# These ensure beam flickers ON then OFF for corresponding values of flux
 
-for i in range(0,tot): #could do step 3
-    
-    # write the comment lines 
-    if flux[i] == '0.0' and i % 3 == 2: #end of line
+for i in range(0, tot):  # could do step 3
+
+    # write the comment lines
+    if flux[i] == '0.0' and i % 3 == 2:  # end of line
         file.write("Beam OFF: "+str(countdays[i])+". seconds\n")
     elif flux[i] != '0.0' and i % 3 == 2:
         file.write("Beam ON: "+str(countdays[i])+". seconds\n")
-    elif flux[i] == '0.0' and i % 3 == 0: # beginning of line
+    elif flux[i] == '0.0' and i % 3 == 0:  # beginning of line
         file.write("* Beam OFF: "+str(countdays[i])+". seconds,")
-    elif flux[i] != '0.0' and i % 3 == 0: # beginning of line
+    elif flux[i] != '0.0' and i % 3 == 0:  # beginning of line
         file.write("* Beam ON: "+str(countdays[i])+". seconds,")
     elif flux[i] == '0.0':
         file.write("Beam OFF: "+str(countdays[i])+". seconds,")
     else:
-        file.write("Beam ON: "+str(countdays[i])+". seconds,")     
+        file.write("Beam ON: "+str(countdays[i])+". seconds,")
 
 file.close()
 
-    
+
 # now write the IRRPROFI lines
-with open("fluka_test.i","r+") as file:
-    lines = file.readlines()             
-    numlines = len(lines) 
-    print("THIS IS NUMLINES=",numlines)
+with open("fluka_test.i", "r+") as file:
+    lines = file.readlines()
+    numlines = len(lines)
+    print("THIS IS NUMLINES=", numlines)
 
     x = -3
-    for j in range(0,numlines):
-        x = x + 3    
-        print("I'm x:",x)
-        if tot % 3 == 1 and x == tot: # end bit
+    for j in range(0, numlines):
+        x = x + 3
+        print("I'm x:", x)
+        if tot % 3 == 1 and x == tot:  # end bit
             irrprofi = str("IRRPROFI"+(20 - (len(str(countdays[x]))+9))*" "
                            +str(countdays[x])+"."+(10-len(str(flux[x])))*" "
                            +str(flux[x]))
-        elif tot % 3 == 2 and x == tot - 1: # end bit
+        elif tot % 3 == 2 and x == tot - 1:  # end bit
             irrprofi = str("IRRPROFI"+(20 - (len(str(countdays[x]))+9))*" "
                            +str(countdays[x])+"."+(10-len(str(flux[x])))*" "
                            +str(flux[x])+(9-len(str(countdays[x+1])))*" "
                            +str(countdays[x+1])+"."+(10-len(str(flux[x+1])))*" "
                            +str(flux[x+1]))
-        else: 
+        else:
             irrprofi = str("IRRPROFI"+(20 - (len(str(countdays[x]))+9))*" "
                            +str(countdays[x])+"."+(10-len(str(flux[x])))*" "
                            +str(flux[x])+(9-len(str(countdays[x+1])))*" "
                            +str(countdays[x+1])+"."+(10-len(str(flux[x+1])))*" "
                            +str(flux[x+1])+(9-len(str(countdays[x+2])))*" "
                            +str(countdays[x+2]) +"."+(10-len(str(flux[x+2])))*" "
-                           +str(flux[x+2])+"\n")   
-        
-        print("hi I'm j:",j)
-        lines.insert(2*j+1,irrprofi)
-    
+                           +str(flux[x+2])+"\n")
+
+        print("hi I'm j:", j)
+        lines.insert(2*j+1, irrprofi)
+
     lines = "".join(lines)
     file.seek(0)
     file.write(lines)
     file.truncate()
-    
-   
 
-
-file.close()        
+file.close()
