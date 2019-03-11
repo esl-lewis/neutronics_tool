6995feaeb76f01e107641e895ed47a46239e0589
esl-lewis <emzlewis@hotmail.co.uk>
2019-03-11 22:27:54 +0000

    Added beam status check for date selection
    Added function which checks whether user has selected a start date when beam was zero, if so this start time is updated to the next nearest date in the future that the beam was on.

--- format_from_EXCEL.py
+++ format_from_EXCEL.py
@@ -7,19 +7,9 @@ needs xlrd package
 import re
 import pandas as pd
 import logging
-import utilities
-
-
-# now also needs to check whether one date is above or below another
-
+import utilities as ut
 import datetime
 
-
-def validate_date(date_text):
-    try:
-        datetime.datetime.strptime(date_text, '%Y-%m-%d')
-    except ValueError:
-        raise ValueError("Incorrect date format, should be YYYY-MM-DD")
 
 
 def get_dates():
@@ -27,11 +17,11 @@ def get_dates():
        cycles we are interested in
     """
     date1 = input("Choose start date with format YYYY-MM-DD: ")
-    validate_date(date1)
+    ut.validate_date(date1)
     date1 = datetime.datetime.strptime(date1,"%Y-%m-%d")
     
     date2 = input("Choose end date with format YYYY-MM-DD: ")
-    validate_date(date2)
+    ut.validate_date(date2)
     date2 = datetime.datetime.strptime(date2,"%Y-%m-%d")
     
     # this will need adjusting between datasets
@@ -39,18 +29,25 @@ def get_dates():
     data_end_date = datetime.datetime.now()
     
     if (date1<data_start_date) or (date2<data_start_date):
-        print("Warning: Date selected is before data collection began.")
+        logging.warning("Warning: Date selected is before data collection began.")
+        # should be complete error, as no need to start before flux 0
+        # logging instead of print warning
+        # if starts on 0, jumps to the next flux non zero. 
+        
     elif (date1>data_end_date) or (date2>data_end_date):
-        print("Warning: Date selected is after todays date.")
+        logging.info("Date selected is after todays date.")
+        # this is OK as cooling time
     
     if date1>date2:
-        print("Start date is before end date. Switched them.")
+        logging.warning("Start date is before end date. Switched them.")
         date_start = date2
         date_end = date1
     else:
         date_start = date1
         date_end = date2
        
+    check_zero(date_start,'cyclemainoperationalparameters.xlsx')
+        
     return [date_start,date_end]
 
 
@@ -63,6 +60,40 @@ def findrng(date1, date2):
     return days
 
 
+# Check if date selected was when beam was off, if so then reset. 
+def check_zero(start_date,file):
+    cols = "B,C"
+    beam_data = pd.read_excel(file, header=None, sheet_name='Data', skiprows=[0,1,2,3,4,5],na_values=['NA'], usecols = cols)
+    beam_data.columns = ["Start", "Finish"]
+    beam_data = beam_data.drop(beam_data.index[86:95])
+
+    beam_data['Dates'] = beam_data.apply(lambda x: findrng(x['Start'], x['Finish']), axis=1)
+    beam_data = beam_data['Dates']
+    
+    # Extract all dates the beam was on and put in list
+    date_list = []
+    for date_range in beam_data:
+        for date in date_range:
+            date_list.append(date)
+
+    # If start_date is in this list then we have a non-zero value and nothing more needed
+    beam_start_status = 'OFF'    
+    for date in date_list:
+        if start_date == date:
+            beam_start_status = 'ON'
+    print('BEAM STATUS:',beam_start_status)
+    
+    # If not, need to jump to next closest date in the future
+    if beam_start_status == 'OFF':
+        for date in date_list:
+            if start_date < date:
+                start_date = date
+                break
+        logging.warning('Start date selected was during beam off time')
+        print('The next closest beam on time was selected instead.')
+        print('New start date=',start_date)
+
+    
 def formatExcel(file):
     """
     Takes data of interest in from excel file and formats to create a pandas
@@ -78,7 +109,6 @@ def formatExcel(file):
     dates = get_dates()
     start_date=dates[0]
     end_date=dates[1]
-  
 
     # Find range in days between start and end points
     rng = pd.date_range(start_date, end_date, freq='D')
@@ -110,14 +140,20 @@ def formatExcel(file):
     """Ensures that empty values are set to zero through combining with an
     empty dataframe"""
 
+
     # chop data frame and only keep relevant data
     final_dataframe = final_dataframe[start_date:end_date]
 
     return final_dataframe
 
+# check for zeroes here or in each individual script. here
+
+   
+
+
 if __name__ == "__main__":
     
-    utilities.setup_logging()
+    ut.setup_logging()
     df2 = formatExcel('cyclemainoperationalparameters.xlsx')
     # select from menu which file to load
-    utilities.plot_irrad(df2)
+    ut.plot_irrad(df2)
