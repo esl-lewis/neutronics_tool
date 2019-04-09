88da680b026e1f589bac9b11c6823510864bf97d
esl-lewis <emzlewis@hotmail.co.uk>
2019-04-09 17:51:20 +0100

    Update testing.py

--- testing.py
+++ testing.py
@@ -8,7 +8,18 @@ import datetime
 file_name = 'cyclemainoperationalparameters.xlsx'
 # Test utilities functions
 
-class Test_get_dates(unittest.TestCase):
 
+class TestUtilities(unittest.TestCase):
+    
+    def test_get_dates_not_none(self):
+        self.assertIsNotNone(ut.get_dates(file_name))
+    
     def test_get_dates_output_type(self):
-        self.assertTrue(isinstance((ut.get_dates(file_name)), datetime.date))
+        self.assertTrue((ut.get_dates(file_name)), datetime.date)
+        
+    #def test_get_dates_input_type(self):
+    #    self.assertEqual(type(file_name),)    
+        
+        
+if __name__ == '__main__':
+    unittest.main()
