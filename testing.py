6d44de99e3d56bb2b1005dd0ff9774c513b95786
esl-lewis <emzlewis@hotmail.co.uk>
2019-05-06 20:51:57 +0100

    Update testing.py
    added current to flux test functions

--- testing.py
+++ testing.py
@@ -6,12 +6,14 @@ import datetime
 import pandas as pd
 #import logging
 
-file_name = 'cyclemainoperationalparameters.xlsx'
+
 # Test utilities functions
 
 
 class TestUtilities(unittest.TestCase):  
-    #def setUp:(self):
+    
+    def setUp(self):
+        file_name = 'cyclemainoperationalparameters.xlsx'
     
     # check something actually gets output
     def test_getdates_notnone(self):
@@ -32,8 +34,6 @@ class TestUtilities(unittest.TestCase):
         
     # insert funcs that test for before and after limits
           
-      
-    
     def test_findrng_type(self):
         testdate1='2012-1-1'
         testdate2='2013-1-1'
@@ -74,22 +74,38 @@ class TestUtilities(unittest.TestCase):
     # checks that fnc does not adjust date when beam was on    
     def test_checkzero_beamONdate(self):
         start_date=datetime.datetime.strptime('2012-02-21',"%Y-%m-%d")
-        file_name = 'cyclemainoperationalparameters.xlsx' 
         self.assertEqual(start_date,ut.check_zero(start_date,file_name))
        
+    def test_formatExcel_output(self):
+        self.assertIsInstance(ut.formatExcel(file_name),pd.DataFrame) 
     
     def test_validatedate_throwsexception(self):
         with self.assertRaises(ValueError):ut.validate_date('2013-ar43v1-1')
     
+    #def test_plotirrad(self):
+    #    mock_df = pd.DataFrame
     
-    """
-    # not currently in use
-    # checks that logging is setup and sends message
-    def test_setuplogging_logs(self):
-        with self.assertLogs('Starting irradiation history generation',level='INFO') as cm:
-            logging.setup_logging
-        self.assertEqual(cm.output)
-    """    
+    def test_currentTOflux_1(self): 
+        self.assertAlmostEqual(ut.currentTOflux(20000),1.25e17)
         
+    def test_currentTOflux_2(self):
+        self.assertAlmostEqual(ut.currentTOflux(0.28419),1.7761875e+12)
+    
+    def test_currentTOflux_3(self):
+        self.assertAlmostEqual(ut.currentTOflux(0),0)
+     
+    def test_currentTOflux_type(self):
+        self.assertIsInstance(ut.currentTOflux(0),int)
+        
+    def test_currentTOflux_type1(self):
+        self.assertIsInstance(ut.currentTOflux(10000),float)
+    
+    # a test for correct type in currentTOflux
+        
+    #def test_formatE(self):
+    #    ut.format_E(0.000324,3,'FLUKA')
+        
+        
+    
 if __name__ == '__main__':
     unittest.main()
