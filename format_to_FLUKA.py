b93336928c50ffa1aabd94b9cbb86551920ffa5a
esl-lewis <emzlewis@hotmail.co.uk>
2019-05-09 17:01:42 +0100

    final updates
    removed extraneous messaging

--- format_to_FLUKA.py
+++ format_to_FLUKA.py
@@ -3,8 +3,6 @@
 import utilities as ut
 import logging
 
-from format_from_EXCEL import formatExcel
-
 
 def FLUKA_output(input_file_name,output_file_name):
     
@@ -41,12 +39,6 @@ def FLUKA_output(input_file_name,output_file_name):
     flux = [ut.round_to_sf(x,4) for x in flux]
     flux = [ut.format_E(x,'FLUKA') for x in flux]
 
-    logging.debug(countdays)
-    logging.debug(flux)
-
-    logging.debug(type(flux[0]))
-    logging.debug(type(flux[1]))
-
     tot = len(countdays)
     
     # write to FLUKA file
@@ -80,7 +72,6 @@ def FLUKA_output(input_file_name,output_file_name):
         lines = file.readlines()
         #print('lines',lines)
         numlines = len(lines)
-        logging.debug("THIS IS NUMLINES= %i", numlines)
 
         x = -3
         for j in range(0, numlines):
@@ -112,7 +103,7 @@ def FLUKA_output(input_file_name,output_file_name):
         file.write(lines)
         file.truncate()
     file.close()
-
+    logging.info("Writing file: %s", output_file_name)
 
 if __name__ == "__main__":
     input_file_name = 'cyclemainoperationalparameters.xlsx'
